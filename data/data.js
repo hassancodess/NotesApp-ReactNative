088988ddc5704e7ff464d614b558ac36b731b965
hassancodess <dev.hassancodes@gmail.com>
2023-02-07 18:16:34 +0500

    Add dummy data

---
+++ data/data.js
@@ -0,0 +1,41 @@
+import uuid from 'react-native-uuid'
+
+const data = [
+  {
+    id: uuid.v4(),
+    title: 'UI concepts worth exsisting',
+    description:
+      'The Design of Everyday Things is required reading for anyone who is interested in the user experience. I personally like to reread it every year or two.Norman is aware of the durability of his work and the applicability of his principles to multiple disciplines. If you know the basics of design better than anyone else, you can apply them flawlessly anywhere.',
+  },
+  {
+    id: uuid.v4(),
+    title: 'Book Review : The Design of Everyday Things by Don Norman',
+    description:
+      'The Design of Everyday Things is required reading for anyone who is interested in the user experience. I personally like to reread it every year or two.Norman is aware of the durability of his work and the applicability of his principles to multiple disciplines. If you know the basics of design better than anyone else, you can apply them flawlessly anywhere.',
+  },
+  {
+    id: uuid.v4(),
+    title: 'Animes produced by Ufotable',
+    description:
+      'The Design of Everyday Things is required reading for anyone who is interested in the user experience. I personally like to reread it every year or two.Norman is aware of the durability of his work and the applicability of his principles to multiple disciplines. If you know the basics of design better than anyone else, you can apply them flawlessly anywhere.',
+  },
+  {
+    id: uuid.v4(),
+    title: 'Mangas planned to read',
+    description:
+      'The Design of Everyday Things is required reading for anyone who is interested in the user experience. I personally like to reread it every year or two.Norman is aware of the durability of his work and the applicability of his principles to multiple disciplines. If you know the basics of design better than anyone else, you can apply them flawlessly anywhere.',
+  },
+  {
+    id: uuid.v4(),
+    title: 'Awesome tweets collection',
+    description:
+      'The Design of Everyday Things is required reading for anyone who is interested in the user experience. I personally like to reread it every year or two.Norman is aware of the durability of his work and the applicability of his principles to multiple disciplines. If you know the basics of design better than anyone else, you can apply them flawlessly anywhere.',
+  },
+  {
+    id: uuid.v4(),
+    title: 'List of free & open source apps',
+    description:
+      'The Design of Everyday Things is required reading for anyone who is interested in the user experience. I personally like to reread it every year or two.Norman is aware of the durability of his work and the applicability of his principles to multiple disciplines. If you know the basics of design better than anyone else, you can apply them flawlessly anywhere.',
+  },
+]
+export default data
