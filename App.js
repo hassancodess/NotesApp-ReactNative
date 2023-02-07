021df46bb4de414518721ed9f3daa77958b5aa4a
hassancodess <dev.hassancodes@gmail.com>
2023-02-07 16:09:21 +0500

    add: fonts and splash screen

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback } from 'react'
+import { useFonts } from 'expo-font'
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import * as SplashScreen from 'expo-splash-screen'
 
-export default function App() {
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync()
+
+const App = () => {
+  const [fontsLoaded] = useFonts({
+    Light: require('./assets/fonts/Nunito-Light.ttf'),
+    Regular: require('./assets/fonts/Nunito-Regular.ttf'),
+    SemiBold: require('./assets/fonts/Nunito-SemiBold.ttf'),
+  })
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded])
+
+  if (!fontsLoaded) {
+    return null
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <>
+      <StatusBar style='dark' />
+      <View onLayout={onLayoutRootView}>
+        <Text style={styles.text}>App</Text>
+      </View>
+    </>
+  )
 }
 
+export default App
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  text: {
+    margin: 102,
+    fontSize: 50,
+    fontFamily: 'Light',
   },
-});
+})
