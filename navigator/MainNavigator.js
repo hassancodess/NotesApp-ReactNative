c032d891e00796ca5e21956802ff02cd8c3ca3fa
hassancodess <dev.hassancodes@gmail.com>
2023-02-07 18:17:03 +0500

    Add Native Stack Navigator

---
+++ navigator/MainNavigator.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+// Screens
+import Home from '../screens/Home'
+import AddNote from '../screens/AddNote'
+import ViewNote from '../screens/ViewNote'
+// Navigator
+const Stack = createNativeStackNavigator()
+
+const MainNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName='Notes'
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: '#252525' },
+        }}
+      >
+        <Stack.Screen name='Notes' component={Home} />
+        <Stack.Screen name='ViewNote' component={ViewNote} />
+        <Stack.Screen name='AddNote' component={AddNote} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default MainNavigator
