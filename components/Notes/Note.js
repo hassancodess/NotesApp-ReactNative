d8ff154c43afd2b6c508a0bf6ddce3a969429283
hassancodess <dev.hassancodes@gmail.com>
2023-02-07 18:42:34 +0500

    add different colors for notes

--- components/Notes/Note.js
+++ components/Notes/Note.js
@@ -1,15 +1,27 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import colors from '../../data/colors'
 
 const Note = ({ note }) => {
   return (
-    <View>
-      <Text>{note.id}</Text>
-      <Text>{note.title}</Text>
+    <View style={[styles.noteContainer(note.color)]}>
+      <Text style={styles.noteTitle}>{note.title}</Text>
     </View>
   )
 }
 
 export default Note
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  noteContainer: (uniqueColor) => ({
+    paddingVertical: 30,
+    paddingHorizontal: 50,
+    marginBottom: 10,
+    borderRadius: 20,
+    backgroundColor: colors[uniqueColor],
+  }),
+  noteTitle: {
+    fontSize: 28,
+    fontFamily: 'Regular',
+  },
+})
